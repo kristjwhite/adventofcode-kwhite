5558fb0a1801ce714c999a74a3a06132fbbd2014
kristjwhite <kristian.white@bbc.co.uk>
2020-12-01 16:04:19 +0000

    tidy up builds

--- Day_1/Part 1/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var inputData = require("./expense.json");
-var expectedSum = 2020;
-var highValues = inputData.foreach(function (expense) {
-    if (expense > expectedSum) {
-        return;
-    }
-});
-console.log("High Values", highValues);
