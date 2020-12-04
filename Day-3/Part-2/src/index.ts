7e284fbfcc7cf54a477e74477ad8d27f467f8e23
kristjwhite <kristian.white@bbc.co.uk>
2020-12-04 08:10:11 +0000

    Part 2 Solution

--- Day-3/Part-2/src/index.ts
+++ Day-3/Part-2/src/index.ts
@@ -1,34 +1,26 @@
-import { isSymbol } from "util";
-
 const fs = require("fs");
 
 const inputData = fs.readFileSync("./data/input.txt", `utf-8`);
+
 const separateLines = inputData.split(/\n/g);
-let indexX: number = 0;
-let isTreeCounter = 0;
-let answer = 0
+
+let answer = 1;
 let movementConfigX: number[] = [1, 3, 5, 7, 1];
 let movementConfigY: number[] = [1, 1, 1, 1, 2];
 
-for (let iteration = 0; iteration <= movementConfigY.length -1 ; iteration++) {
-  console.log(
-    "Checking Config; Right:",
-    movementConfigX[iteration],
-    " Down: ",
-    movementConfigY[iteration]
-  );
-
+for (let n = 0; n <= movementConfigX.length - 1; n++) {
   answer =
-    treeHittingCalculator(
-      movementConfigX[iteration],
-      movementConfigY[iteration]
-    );
+    answer * treeHittingCalculator(movementConfigX[n], movementConfigY[n]);
+
   console.log("Answer so far: ", answer);
 }
 console.log("Answer", answer);
 
 function treeHittingCalculator(rightMove: number, downMove: number) {
-  for (let i = 1; i <= separateLines.length - 1; i + downMove) {
+  let isTreeCounter = 0;
+  let indexX: number = 0;
+  console.log("Checking Config; Right:", rightMove, " Down: ", downMove);
+  for (let i = downMove; i <= separateLines.length - 1; i = i + downMove) {
     indexX = indexX + rightMove;
     if (indexX > 30) {
       indexX = indexX - 31;
@@ -37,6 +29,6 @@ function treeHittingCalculator(rightMove: number, downMove: number) {
       isTreeCounter++;
     }
   }
-
+  console.log("Answer this iteration is ", isTreeCounter);
   return isTreeCounter;
 }
