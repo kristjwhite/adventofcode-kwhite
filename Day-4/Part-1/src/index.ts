7046849d838606a89f245a994268eb2352e4b720
kristjwhite <kristian.white@bbc.co.uk>
2020-12-04 17:43:30 +0000

    part one compelted

--- Day-4/Part-1/src/index.ts
+++ Day-4/Part-1/src/index.ts
@@ -1,22 +1,60 @@
-import { isSymbol } from "util";
-
 const fs = require("fs");
 
 const inputData = fs.readFileSync("./data/input.txt", `utf-8`);
-const separateLines = inputData.split(/\n/g);
-let indexX: number = 0;
-let isTreeCounter = 0;
+
+ interface Passport {
+  ecl: string;
+  pid: string;
+  eyr: string;
+  hcl: string;
+  byr: string;
+  iyr: string;
+  hgt: string;
+  cid?: string;
+};
 
 
+let separateData: string = inputData.split("\n\n");
+let cleanedData: any = [];
+let jsonParsed: any = [];
+let validCounter:number = 0
+for (let i = 0; i < separateData.length-1; i++) {
+  cleanedData[i] = separateData[i].replace(/(\r\n|\n|\r)/gm, " ");
 
-  for (let i = 1; i <= separateLines.length - 1; i++) {
-    indexX = indexX + 3;
-    if (indexX > 30) {
-      indexX = indexX - 31;
-    }
-    if (separateLines[i].charAt(indexX) == "#") {
-      isTreeCounter++;
-    }
+  jsonParsed = (getJsonData(cleanedData[i]));
+  let isValid = isPassport(jsonParsed)
+  if( isValid == true){
+    validCounter++
   }
+  console.log("Valid Passport: ", isValid);
+}
+console.log("Total Valid = ", validCounter)
 
-console.log("Answer: ", isTreeCounter)
+
+
+function isPassport(obj: any): obj is Passport {
+  return (
+      obj &&
+      typeof obj.ecl && typeof(obj.ecl) == 'string' &&
+      typeof obj.pid && typeof(obj.pid) == 'string' &&
+      typeof obj.eyr && typeof(obj.eyr) == 'string' &&
+      typeof obj.hcl && typeof(obj.hcl) == 'string' &&
+      typeof obj.byr && typeof(obj.byr) == 'string' &&
+      typeof obj.iyr && typeof(obj.iyr) == 'string' &&
+      typeof obj.hgt && typeof(obj.hgt) == 'string'
+  );
+}
+
+function getJsonData(query: string) {
+  let arrayOfKeyValues = query.split(/\s/g);
+  let modifiedArray = new Array();
+  for (let i = 0; i < arrayOfKeyValues.length; i++) {
+    let arrayValues = arrayOfKeyValues[i].split(":");
+    let arrayString =
+      '"' + arrayValues[0] + '"' + ":" + '"' + arrayValues[1] + '"';
+    modifiedArray.push(arrayString);
+  }
+  let jsonDataString = "{" + modifiedArray.toString() + "}";
+  let jsonData = JSON.parse(jsonDataString);
+  return jsonData;
+}
