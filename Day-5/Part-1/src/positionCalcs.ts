c5530e08217bfb90362c690159bf20305e1b2943
kristjwhite <kristian.white@bbc.co.uk>
2020-12-05 12:00:57 +0000

    WIP day 5

---
+++ Day-5/Part-1/src/positionCalcs.ts
@@ -0,0 +1,39 @@
+export function rowCalc(val: string) {
+  let row: string = val.substring(0, 6);
+  let position: number[] = [0, 127];
+  let lastPicked: number = 0;
+  for (let i = 0; i < row.length; i++) {
+    if (row.charAt(i) == "F") {
+      position[1] = position[1] - Math.floor((position[1] - position[0]) / 2);
+      lastPicked = position[1];
+    }
+    if (row.charAt(i) == "B") {
+      position[0] = position[0] + Math.ceil((position[1] - position[0]) / 2);
+      lastPicked = position[0];
+    }
+  }
+  return lastPicked;
+}
+
+export function colCalc(val: string) {
+    let col: string = val.slice(-3);
+    let position: number[] = [0, 7];
+    let lastPicked: number = 0;
+    for (let i = 0; i < col.length; i++) {
+      if (col.charAt(i) == "L") {
+        position[1] = position[1] - Math.floor((position[1] - position[0]) / 2);
+        lastPicked = position[1]
+        if(position[0] <= position[1]){
+            lastPicked  = position[0]
+        };
+      }
+      if (col.charAt(i) == "R") {
+        position[0] = position[0] + Math.ceil((position[1] - position[0]) / 2);
+        lastPicked = position[0];
+        if(position[1] >= position[0]){
+            lastPicked  = position[1]
+        };
+      }
+    }
+    return lastPicked;
+  }
