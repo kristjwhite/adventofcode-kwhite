6c128ab444759dd83592be9ed448b1f87f354818
kristjwhite <kristian.white@bbc.co.uk>
2020-12-07 09:08:57 +0000

    Finish Part 1

--- Day-6/Part-1/src/index.ts
+++ Day-6/Part-1/src/index.ts
@@ -2,7 +2,29 @@ const fs = require("fs");
 
 const inputData = fs.readFileSync("./data/input.txt", `utf-8`);
 
-let separateGroups: string = inputData.split("\n\n");
+let separateGroups: string[] = inputData.split("\n\n");
+let sumAnswer = 0
 
 console.log("Groups split: ", separateGroups[0])
 
+separateGroups.forEach((value,index) => {
+    separateGroups[index] = value.replace(/\n/g, "")
+});
+console.log("Cleaned Groups split: ", separateGroups[0])
+
+separateGroups.forEach((value,index) => {
+
+
+ sumAnswer = sumAnswer + removeDuplicateCharacters(value).length
+});
+
+console.log("Sum Total", sumAnswer)
+
+export function removeDuplicateCharacters(string: string) {
+    return string
+      .split('')
+      .filter(function(item, pos, self) {
+        return self.indexOf(item) == pos;
+      })
+      .join('');
+  }
--- Day-6/Part-1/test/positionCalcs.test.ts
+++ Day-6/Part-1/test/positionCalcs.test.ts
@@ -1,43 +1,13 @@
-import * as calcs from '../src/positionCalcs'
+import * as func from '../src/functions'
 
 
 
 describe('Row Checker', ()=>{
-    const testData:any[] = [
-        ["BFFFBBFRRR", 70],
-        ["FFFBBBFRRR", 14],
-        ["BBFFBBFRLL", 102]
-    ]
+    const testData:any[] = [["abcdef",6], ["aaaa",1], ["abcabc", 3]]
     it.each(testData)("should eval to the right row" ,(testData,correctAnswer)=>{
-        const check = calcs.rowCalc(testData)
-
-        expect(check).toEqual(correctAnswer)
-    })
-})
-describe('Row Checker', ()=>{
-    const testData:any[] = [
-        ["BFFFBBFRRR", 7],
-        ["FFFBBBFRRR", 7],
-        ["BBFFBBFRLL", 4]
-    ]
-    it.each(testData)("should eval to the right row" ,(testData,correctAnswer)=>{
-        const check = calcs.colCalc(testData)
+        const check = func.uniqueCharCount(testData)
 
         expect(check).toEqual(correctAnswer)
     })
 })
 
-describe('Boarding Pass Calc', ()=>{
-    const testData:any[] = [
-        ["BFFFBBFRRR", 567],
-        ["FFFBBBFRRR", 119],
-        ["BBFFBBFRLL", 820],
-        ["FFFFFFFLLL", 8]
-    ]
-    it.each(testData)("should eval to the right row" ,(testData,correctAnswer)=>{
-        let col = calcs.colCalc(testData)
-        let row = calcs.rowCalc(testData)
-        let passNumber: number = (row * 8) + col;
-        expect(passNumber).toEqual(correctAnswer)
-    })
-})
